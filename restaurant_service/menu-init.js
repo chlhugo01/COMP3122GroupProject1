e5f1bf1cd539fc6f0ddb21cdda0967834c4abed5
chlhugo01 <59417093+chlhugo01@users.noreply.github.com>
2021-11-11 15:59:17 +0800

    Update restauarnt_service

--- restaurant_service/menu-init.js
+++ restaurant_service/menu-init.js
@@ -6,3 +6,4 @@ db.createCollection('menu');
 db.order.insertOne({'restaurant_id':1,'Food_id':1,'Food_Name':'Potato', 'Food_Price': 10});
 db.order.insertOne({'restaurant_id':1,'Food_id':2,'Food_Name':'Tomato', 'Food_Price': 15});
 db.order.insertOne({'restaurant_id':1,'Food_id':3,'Food_Name':'Fish', 'Food_Price': 20});
+
--- restaurant_service/restaurant_service.py
+++ restaurant_service/restaurant_service.py
@@ -1,9 +1,50 @@
-from flask import request, Flask, jsonify
+from flask import Flask, jsonify, request, redirect, url_for, render_template
 from pymongo import MongoClient
+import pytest
+import requests
+import time
+import os
 
 app = Flask(__name__)
-myclient = MongoClient('mongodb://comp3122:23456@mongo:27104')
+client = MongoClient('mongodb://comp3122:23456@mongo:27104')
+store = client.Order.Store.find()
+storearr = []
+for doc in store:
+    storearr.append(doc)
+temp6 = []
+for user in storearr:
+    temp6.append({
+        'id': int(user["id"]),
+        'name': user["name"],
+        'address': user["address"],
+        # 'gpa' : user["gpa"]
+    })
 
-# start flask server
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=15104)
+
+@app.route('/')
+def todo():
+    return jsonify({"Student ID": "190dfdsfd7d", "Name": "Lee Chun Hang"}), 200
+
+
+@app.route('/store')
+def me():
+    #return jsonify(menuarray),200
+    return render_template('store.html', storearr=temp6)
+
+
+@app.route('/store/<store_id>')
+def store_name(store_id):
+    menuresult = client.Order.FoodMenu.find({"store_id": int(store_id)})
+    menuarray=[]
+    for i in menuresult:
+        menuarray.append({
+        'store_id': i["store_id"],
+        'food_id': i["food_id"],
+        'food_name': i["food_name"],
+        'food_nrice': i["food_price"]
+        })
+    #return jsonify(menuarray),200
+    return render_template('menu.html', menu=menuarray)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=15000)
