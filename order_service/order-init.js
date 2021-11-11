18604a0f033582c21390e649335441f9c08b6c1e
him <18062182A@student.hkcc-polyu.edu.hk>
2021-11-11 18:06:03 +0800

    update order

--- order_service/order-init.js
+++ order_service/order-init.js
@@ -6,10 +6,11 @@ db.createCollection('order');
 db.order.insertOne({'order_id': 1, 'customer_id': 1,'store_id': 1,'delivery_id': 1});
 db.order.insertOne({'order_id': 2, 'customer_id': 2,'store_id': 1,'delivery_id': 2});
 db.order.insertOne({'order_id': 3, 'customer_id': 2,'store_id': 3,'delivery_id': 3});
-
+db.order.insertOne({'order_id': 4, 'customer_id': 3,'store_id': 2,'delivery_id': 3});
 
 db.createCollection('detail');
 db.detail.insertOne({'order_id': 1, 'food_id': 1, 'number': 1});
 db.detail.insertOne({'order_id': 1, 'food_id': 2, 'number': 2});
 db.detail.insertOne({'order_id': 2, 'food_id': 1, 'number': 2});
 db.detail.insertOne({'order_id': 3, 'food_id': 1, 'number': 1});
+db.detail.insertOne({'order_id': 4, 'food_id': 3, 'number': 2});
