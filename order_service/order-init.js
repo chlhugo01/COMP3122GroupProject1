2965dc8e66f99678c0f2b7f0ad8e47c9b582e471
him <18062182A@student.hkcc-polyu.edu.hk>
2021-11-11 15:05:39 +0800

    order database change

--- order_service/order-init.js
+++ order_service/order-init.js
@@ -1,5 +1,15 @@
 db.auth('comp3122', '23456')
-db = db.getSiblingDB('order')
+db = db.getSiblingDB('Order')
 
-db.createCollection('record');
-db.admin.insertOne({'record_id':1, 'costomer_id':1, 'restaurant_id':1, 'delivery_id':1});
+db.createCollection('order');
+
+db.order.insertOne({'order_id':00001, 'customer_id':1,'store_id':1,'delivery_id':1});
+db.order.insertOne({'order_id':00002, 'customer_id':2,'store_id':1,'delivery_id':2});
+db.order.insertOne({'order_id':00003, 'customer_id':2,'store_id':3,'delivery_id':3});
+
+
+db.createCollection('detail');
+db.detail.insertOne({'order_id': 00001, 'food_id': 1, 'number':1});
+db.detail.insertOne({'order_id': 00001, 'food_id': 2, 'number':2});
+db.detail.insertOne({'order_id': 00002, 'food_id': 1, 'number':2});
+db.detail.insertOne({'order_id': 00003, 'food_id': 1, 'number':1});
