5bf267b1bf123c94813f5e0016837a94d91cf65a
chlhugo01 <59417093+chlhugo01@users.noreply.github.com>
2021-11-12 04:03:36 +0800

    Update login, restaurant and order service
    The whole docker file can run now

--- order_service/order-init.js
+++ order_service/order-init.js
@@ -3,10 +3,10 @@ db = db.getSiblingDB('Order')
 
 db.createCollection('order');
 
-db.order.insertOne({'order_id': 1, 'customer_id': 1,'store_id': 1,'delivery_id': 1});
-db.order.insertOne({'order_id': 2, 'customer_id': 2,'store_id': 1,'delivery_id': 2});
-db.order.insertOne({'order_id': 3, 'customer_id': 2,'store_id': 3,'delivery_id': 3});
-db.order.insertOne({'order_id': 4, 'customer_id': 3,'store_id': 2,'delivery_id': 3});
+db.order.insertOne({'order_id': 1, 'customer_id': 1,'restaurant_id': 1,'delivery_id': 1});
+db.order.insertOne({'order_id': 2, 'customer_id': 2,'restaurant_id': 1,'delivery_id': 2});
+db.order.insertOne({'order_id': 3, 'customer_id': 2,'restaurant_id': 3,'delivery_id': 3});
+db.order.insertOne({'order_id': 4, 'customer_id': 3,'restaurant_id': 2,'delivery_id': 3});
 
 db.createCollection('detail');
 db.detail.insertOne({'order_id': 1, 'food_id': 1, 'number': 1});
