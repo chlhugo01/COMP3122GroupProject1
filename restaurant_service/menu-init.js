30b6ff09d037d32cd13300e0508fd609eaf4fca3
him <18062182A@student.hkcc-polyu.edu.hk>
2021-11-11 15:46:49 +0800

    update menu

---
+++ restaurant_service/menu-init.js
@@ -0,0 +1,8 @@
+db.auth('comp3122', '23456')
+db = db.getSiblingDB('Menu')
+
+db.createCollection('menu');
+
+db.order.insertOne({'restaurant_id':1,'Food_id':1,'Food_Name':'Potato', 'Food_Price': 10});
+db.order.insertOne({'restaurant_id':1,'Food_id':2,'Food_Name':'Tomato', 'Food_Price': 15});
+db.order.insertOne({'restaurant_id':1,'Food_id':3,'Food_Name':'Fish', 'Food_Price': 20});
