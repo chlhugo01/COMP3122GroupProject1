bf0fcd5feec7f05dc4102583226d4800bf8de088
tung9898 <hoyiutung@hotmail.com>
2021-11-13 01:38:26 +0800

    [feature] Set up order service

--- order_service/order_service.py
+++ order_service/order_service.py
@@ -6,23 +6,35 @@ import time
 import os
 
 app = Flask(__name__)
-client = MongoClient('mongodb://comp3122:23456@db2:27017')
+orderdb = MongoClient('mongodb://comp3122:23456@db2:27017')
+restaurantdb = MongoClient('mongodb://comp3122:23456@db1:27017')
 
-@app.route('/')
-def todo():
-    return jsonify({"Student ID": "190dfdsfd7d", "Name": "Lee Chun Hang"}), 200
+@app.route('/', methods=['GET'])
+def welcome():
+    return jsonify({"Welcome Message": "Hello, order"}), 200
 
-@app.route('/Order/<order_id>')
-def store_name(order_id):
-    orderresult = client.Order.order.find({"order_id":int(order_id)})
-    order = []
-    for i in orderresult:
-        order.append({
-            'customer_id': i["customer_id"],
-            'restaurant_id':i["restaurant_id"],
-            'delivery_id': i['delivery_id']
-        })
-    return jsonify(order),200
+@app.route('/order', methods=['GET'])
+def get_menu():
+    allmenu = []
+    for i in restaurantdb.Restaurant.restaurants.find({},{"_id": 0}).sort("id"):
+        menu = {'name': i['name'], 'address': i['address'], 'phone_number': int(i['phone_number'])}
+        for j in restaurantdb.Restaurant.menu.find({},{"_id": 0}).sort("id"):
+            if i['id'] == j['restaurant_id']:
+                menu[j['name']] = j['price']
+        allmenu.append(menu)
+    return jsonify(allmenu), 200
+
+@app.route('/order/<order_id>')
+def order(order_id):
+    # orderresult = orderdb.Order.order.find({"order_id":int(order_id)})
+    # order = []
+    # for i in orderresult:
+    #     order.append({
+    #         'customer_id': i["customer_id"],
+    #         'restaurant_id':i["restaurant_id"],
+    #         'delivery_id': i['delivery_id']
+    #     })
+    return jsonify(order), 200
 
 
 if __name__ == "__main__":
--- restaurant_service/restaurant-init.js
+++ restaurant_service/restaurant-init.js
@@ -1,17 +1,18 @@
 db.auth('comp3122', '23456')
-db = db.getSiblingDB('Menu')
+db = db.getSiblingDB('Restaurant')
+
+db.createCollection('restaurants');
+db.restaurants.insertOne({'id': 1, 'name': 'hk happy meal 1', 'address': 'Hong Kong A', 'phone_number': 12345678});
+db.restaurants.insertOne({'id': 2, 'name': 'hk happy meal 2', 'address': 'Hong Kong B', 'phone_number': 23231478});
+db.restaurants.insertOne({'id': 3, 'name': 'hk happy meal 3', 'address': 'Hong Kong C', 'phone_number': 54326728});
 
 db.createCollection('menu');
-
-db.menu.insertOne({'restaurant_id':1,'Food_id':1,'Food_Name':'Fishball', 'Food_Price': 5});
-db.menu.insertOne({'restaurant_id':1,'Food_id':2,'Food_Name':'Siu mai', 'Food_Price': 7});
-db.menu.insertOne({'restaurant_id':1,'Food_id':3,'Food_Name':'Rice flour roll', 'Food_Price': 12});
-
-db.menu.insertOne({'restaurant_id':2,'Food_id':1,'Food_Name':'Chicken', 'Food_Price': 40});
-db.menu.insertOne({'restaurant_id':2,'Food_id':2,'Food_Name':'Pork', 'Food_Price': 45});
-db.menu.insertOne({'restaurant_id':2,'Food_id':3,'Food_Name':'Beaf', 'Food_Price': 50});
-
-db.menu.insertOne({'restaurant_id':3,'Food_id':1,'Food_Name':'Curry rice', 'Food_Price': 35});
-db.menu.insertOne({'restaurant_id':3,'Food_id':2,'Food_Name':'Hamburger', 'Food_Price': 30});
-db.menu.insertOne({'restaurant_id':3,'Food_id':3,'Food_Name':'Ramen', 'Food_Price': 40});
-
+db.menu.insertOne({'id': 1, 'restaurant_id':1, 'name':'Fishball', 'price': 5});
+db.menu.insertOne({'id': 2, 'restaurant_id':1, 'name':'Siu mai', 'price': 7});
+db.menu.insertOne({'id': 3, 'restaurant_id':1, 'name':'Rice flour roll', 'price': 12});
+db.menu.insertOne({'id': 4, 'restaurant_id':2, 'name':'Chicken', 'price': 40});
+db.menu.insertOne({'id': 5, 'restaurant_id':2, 'name':'Pork', 'price': 45});
+db.menu.insertOne({'id': 6, 'restaurant_id':2, 'name':'Beaf', 'price': 50});
+db.menu.insertOne({'id': 7, 'restaurant_id':3, 'name':'Curry rice', 'price': 35});
+db.menu.insertOne({'id': 8, 'restaurant_id':3, 'name':'Hamburger', 'price': 30});
+db.menu.insertOne({'id': 9, 'restaurant_id':3, 'name':'Ramen', 'price': 40});
