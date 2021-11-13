fee0c70f9d6976b4b1fccef1e8a3e67588c87379
tung9898 <hoyiutung@hotmail.com>
2021-11-13 15:26:24 +0800

    [fix] Added db variable "status"

--- order_service/order-init.js
+++ order_service/order-init.js
@@ -2,10 +2,10 @@ db.auth('comp3122', '23456')
 db = db.getSiblingDB('order')
 
 db.createCollection('receipts');
-db.receipts.insertOne({'id': 1, 'customer_id': 0,'restaurant_id': 1});
-db.receipts.insertOne({'id': 2, 'customer_id': 0,'restaurant_id': 1});
-db.receipts.insertOne({'id': 3, 'customer_id': 0,'restaurant_id': 3});
-db.receipts.insertOne({'id': 4, 'customer_id': 0,'restaurant_id': 2});
+db.receipts.insertOne({'id': 1, 'customer_id': 0,'restaurant_id': 1, 'status': 'finished'});
+db.receipts.insertOne({'id': 2, 'customer_id': 0,'restaurant_id': 1, 'status': 'finished'});
+db.receipts.insertOne({'id': 3, 'customer_id': 0,'restaurant_id': 3, 'status': 'finished'});
+db.receipts.insertOne({'id': 4, 'customer_id': 0,'restaurant_id': 2, 'status': 'finished'});
 
 db.createCollection('details');
-db.details.insertOne({'id': 1, 'receipts_id': 1, 'food_id': 1, 'number': 1, 'price': 5});
+db.details.insertOne({'id': 1, 'receipt_id': 1, 'food_id': 1, 'number': 1, 'price': 5});
