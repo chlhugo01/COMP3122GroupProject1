d5063f369fc1f3be044bef8e8d812534a541e921
him <18062182A@student.hkcc-polyu.edu.hk>
2021-11-13 12:15:26 +0800

    change id name of menu db

--- restaurant_service/restaurant-init.js
+++ restaurant_service/restaurant-init.js
@@ -7,14 +7,14 @@ db.restaurants.insertOne({'id': 2, 'name': 'hk happy meal 2', 'address': 'Hong K
 db.restaurants.insertOne({'id': 3, 'name': 'hk happy meal 3', 'address': 'Hong Kong C', 'phone_number': 54326728});
 
 db.createCollection('menu');
-db.menu.insertOne({'restaurant_id':1, 'food_id': 1,'name':'Fishball', 'price': 5});
-db.menu.insertOne({'restaurant_id':1, 'food_id': 2,'name':'Siu mai', 'price': 7});
-db.menu.insertOne({'restaurant_id':1, 'food_id': 3,'name':'Rice flour roll', 'price': 12});
+db.menu.insertOne({'restaurant_id':1, 'id': 1,'name':'Fishball', 'price': 5});
+db.menu.insertOne({'restaurant_id':1, 'id': 2,'name':'Siu mai', 'price': 7});
+db.menu.insertOne({'restaurant_id':1, 'id': 3,'name':'Rice flour roll', 'price': 12});
 
-db.menu.insertOne({'restaurant_id':2, 'food_id': 1,'name':'Chicken', 'price': 40});
-db.menu.insertOne({'restaurant_id':2, 'food_id': 2,'name':'Pork', 'price': 45});
-db.menu.insertOne({'restaurant_id':2, 'food_id': 3,'name':'Beaf', 'price': 50});
+db.menu.insertOne({'restaurant_id':2, 'id': 1,'name':'Chicken', 'price': 40});
+db.menu.insertOne({'restaurant_id':2, 'id': 2,'name':'Pork', 'price': 45});
+db.menu.insertOne({'restaurant_id':2, 'id': 3,'name':'Beaf', 'price': 50});
 
-db.menu.insertOne({'restaurant_id':3, 'food_id': 1, 'name':'Curry rice', 'price': 35});
-db.menu.insertOne({'restaurant_id':3, 'food_id': 2, 'name':'Hamburger', 'price': 30});
-db.menu.insertOne({'restaurant_id':3, 'food_id': 3, 'name':'Ramen', 'price': 40});
+db.menu.insertOne({'restaurant_id':3, 'id': 1, 'name':'Curry rice', 'price': 35});
+db.menu.insertOne({'restaurant_id':3, 'id': 2, 'name':'Hamburger', 'price': 30});
+db.menu.insertOne({'restaurant_id':3, 'id': 3, 'name':'Ramen', 'price': 40});
