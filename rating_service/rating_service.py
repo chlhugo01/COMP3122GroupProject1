997b31cfb8cd83bc762062e90dfe218c69ba2006
Justin Cheung <i.am@justincue.ng>
2021-11-13 18:06:13 +0800

    [refactor] delete unused microservices

--- rating_service/rating_service.py
+++
@@ -1,9 +0,0 @@
-from flask import request, Flask, jsonify
-from pymongo import MongoClient
-
-app = Flask(__name__)
-myclient = MongoClient('mongodb://comp3122:23456@mongo:27108')
-
-# start flask server
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=15108)
